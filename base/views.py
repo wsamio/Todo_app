6a39fef99deadc0ea8f75febec6804cb3518b227
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-28 07:07:22 -0700

    update

--- base/views.py
+++ base/views.py
@@ -5,6 +5,16 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
+from django.contrib.auth.views import LoginView
+
+
+class CustomLogin(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_autheticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
@@ -30,3 +40,4 @@ class TaskDelete(DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
