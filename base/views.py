a0f14a0302eb5ec942980d722edc7613ccb23f7a
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-28 02:03:16 -0700

    update

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def tasklist(request):
-    return HttpResponse('Todo App')
+
+class TaskList(ListView):
+    model = Task
